b14c05ce22831546ba0bd1a4b148e4feea339dd1
candra <selubungers@gmail.com>
2016-12-25 21:48:32 +0700

    fix fan

--- application/views/home/homepage/service.php
+++ application/views/home/homepage/service.php
@@ -128,8 +128,7 @@
         <div class="centerFan">
           <div class="item1"></div>
           <div class="item2"></div>
-          <div class="item3"></div><!-- 
-          <div class="item4"></div> -->
+          <div class="item3"></div>
         </div>
         <div class="fan-top"></div>
       </div>
