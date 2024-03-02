4908af295c3358c9542b6c52643fc41f976d1ccd
Candra Dwi Prasetyo <candra.dwi@wartek.belajar.id>
2024-03-02 12:12:35 +0700

    update style

--- application/views/home/homepage/banner.php
+++ application/views/home/homepage/banner.php
@@ -29,44 +29,44 @@
         <div class="tree-group1">
           <svg width="150px" height="150px" class="svg-tree0">
             <g id="tree" class="tree3">
-              <image xlink:href="<? base_url()?>assets/images/tree3.png" x="0" y="0" height="150px" width="150px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree3.png" x="0" y="0" height="150px" width="150px"/>         
             </g>
           </svg>
            <svg width="387px" height="317px" class="svg-tree">
             <g id="tree" class="tree">
-              <image xlink:href="<? base_url()?>assets/images/tree.png" x="0" y="0" height="387px" width="317px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree.png" x="0" y="0" height="387px" width="317px"/>         
             </g>
           </svg>
           <svg width="80px" height="80px" class="svg-tree2">
             <g id="tree" class="tree">
-              <image xlink:href="<? base_url()?>assets/images/tree2.png" x="0" y="0" height="80px" width="80px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree2.png" x="0" y="0" height="80px" width="80px"/>         
             </g>
           </svg>
           <svg width="111px" height="61px" class="svg-root">
             <g>
-              <image xlink:href="<? base_url()?>assets/images/root.png" x="0" y="0" height="61px" width="111px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/root.png" x="0" y="0" height="61px" width="111px"/>         
             </g>
           </svg>
           <svg width="100px" height="100px" class="svg-tree6">
             <g id="tree" class="tree4">
-              <image xlink:href="<? base_url()?>assets/images/tree3.png" x="0" y="0" height="100px" width="100px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree3.png" x="0" y="0" height="100px" width="100px"/>         
             </g>
           </svg>
           <svg width="150px" height="150px" class="svg-tree3">
             <g id="tree" class="tree2">
-              <image xlink:href="<? base_url()?>assets/images/tree3.png" x="0" y="0" height="150px" width="150px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree3.png" x="0" y="0" height="150px" width="150px"/>         
             </g>
           </svg>
 
           <svg width="200px" height="200px" class="svg-tree4">
             <g id="tree" class="tree3">
-              <image xlink:href="<? base_url()?>assets/images/tree4.png" x="0" y="0" height="200px" width="200px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree4.png" x="0" y="0" height="200px" width="200px"/>         
             </g>
           </svg>
 
            <svg width="100px" height="100px" class="svg-tree5">
             <g id="tree" class="tree4">
-              <image xlink:href="<? base_url()?>assets/images/tree3.png" x="0" y="0" height="100px" width="100px"/>         
+              <image xlink:href="<?php base_url()?>assets/images/tree3.png" x="0" y="0" height="100px" width="100px"/>         
             </g>
           </svg>
         </div>
